94ec39bec880d8602b3696f3288ff0a0c01a22b1
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-12 15:38:20 +0200

    Add test for dto "class POJO"

--- src/main/java/com/example/quiz/HealthcheckRestController.java
+++ src/main/java/com/example/quiz/HealthcheckRestController.java
@@ -1,15 +1,21 @@
 package com.example.quiz;
 
+import com.example.quiz.dto.HealthCheckDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/helath")
 public class HealthcheckRestController {
 
     @GetMapping
-    public String healthcheck(){
-        return "It'working";
+   public HealthCheckDto healthCheck(){
+        HealthCheckDto dto = new HealthCheckDto(true,"Working now");
+        return dto;
     }
+
 }
